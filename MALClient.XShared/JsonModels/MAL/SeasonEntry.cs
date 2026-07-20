5210d30e94c4ddc7b0b4ecef02c5f3d015637ee3
stefan9999991 <stefan-2013-2014@hotmail.com>
2026-07-20 18:12:16 +0200

    Make season data use MAL api instead of Jikan for reliable output again

---
+++ MALClient.XShared/JsonModels/MAL/SeasonEntry.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MALClient.XShared.JsonModels.MAL
+{
+    internal class SeasonEntry
+    {
+        [JsonPropertyName("id")]
+        public long? MalId { get; set; }
+
+        [JsonPropertyName("title")]
+        public string Title { get; set; }
+
+        [JsonPropertyName("main_picture")]
+        public MainPicture Picture { get; set; }
+
+        [JsonPropertyName("num_episodes")]
+        public long? Episodes { get; set; }
+
+        [JsonPropertyName("mean")]
+        public double? Score { get; set; }
+
+        [JsonPropertyName("genres")]
+        public ICollection<Genre> Genres { get; set; }
+
+        [JsonPropertyName("num_list_users")]
+        public long? MembersCount { get; set; }
+
+        [JsonPropertyName("start_season")]
+        public Season StartSeason { get; set; }
+    }
+}
