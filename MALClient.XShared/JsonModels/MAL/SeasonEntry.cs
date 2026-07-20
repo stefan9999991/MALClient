24c91fd23eda36aed3392d849cb4e6b135a31293
stefan9999991 <stefan-2013-2014@hotmail.com>
2026-07-20 23:49:49 +0200

    Use the MAL api for anime entries to make it work all the time again

--- MALClient.XShared/JsonModels/MAL/SeasonEntry.cs
+++ MALClient.XShared/JsonModels/MAL/SeasonEntry.cs
@@ -5,30 +5,7 @@ using System.Text;
 
 namespace MALClient.XShared.JsonModels.MAL
 {
-    internal class SeasonEntry
+    internal class SeasonEntry : AnimeEntry
     {
-        [JsonPropertyName("id")]
-        public long? MalId { get; set; }
-
-        [JsonPropertyName("title")]
-        public string Title { get; set; }
-
-        [JsonPropertyName("main_picture")]
-        public MainPicture Picture { get; set; }
-
-        [JsonPropertyName("num_episodes")]
-        public long? Episodes { get; set; }
-
-        [JsonPropertyName("mean")]
-        public double? Score { get; set; }
-
-        [JsonPropertyName("genres")]
-        public ICollection<Genre> Genres { get; set; }
-
-        [JsonPropertyName("num_list_users")]
-        public long? MembersCount { get; set; }
-
-        [JsonPropertyName("start_season")]
-        public Season StartSeason { get; set; }
     }
 }
